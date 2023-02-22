673347372268824e8bd726d84043c67cad536f2d
Krzysiek <krzysztof.g.git@gmail.com>
2023-02-22 19:40:10 +0100

    All app to test

---
+++ crypto.py
@@ -0,0 +1,65 @@
+from abc import ABC
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+
+class Execute(ABC):
+    def execute(self, password):
+        pass
+
+
+class EncryptDecrypt(Execute):
+    def execute(self, password):
+        pass
+
+    def __init__(self, path):
+        self.path = path
+
+    @staticmethod
+    def create_key(password):
+        salt = b'\xda\x01\xac\x87\xe9<\xef\x13\xdd\xa2\x08\xce\xbd\xbb\x93\xd7'
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=390000)
+        key = base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
+        return key
+
+
+class Decryption(EncryptDecrypt):
+    def execute(self, password):
+        with open(self.path, 'r') as file:
+            data_to_decrypt = file.read()
+        fernet = Fernet(self.create_key(password))
+        decrypt_content = fernet.decrypt(data_to_decrypt.encode('utf-8'))
+        with open(self.path.rename(self.path.with_suffix('.txt')), 'w') as file:
+            file.write(decrypt_content.decode('utf-8'))
+
+
+class Encryption(EncryptDecrypt):
+    def execute(self, password):
+        with open(self.path, 'r') as file:
+            data_to_encrypt = file.read()
+        fernet = Fernet(self.create_key(password))
+        encrypted_content = fernet.encrypt(data_to_encrypt.encode('utf-8'))
+        with open(self.path.rename(self.path.with_suffix('.secret')), 'w') as file:
+            file.write(encrypted_content.decode('utf-8'))
+
+
+class Append(EncryptDecrypt):
+    def __init__(self, path, text):
+        self.text = text.encode('utf-8')
+        super().__init__(path)
+
+    def execute(self, password):
+        # red file
+        with open(self.path, 'r') as file:
+            data = file.read()
+        # decrypt file
+        fernet = Fernet(self.create_key(password))
+        decrypted_contend = fernet.decrypt(data.encode('utf-8'))
+        # append text
+        decrypted_contend += b'\n'
+        decrypted_contend += self.text
+        # encryption file
+        with open(self.path, 'w') as file:
+            file.write(decrypted_contend.decode('utf-8'))
---
+++ main.py
@@ -0,0 +1,99 @@
+"""Application to encrypt and decrypt single txt file or all select directory ( only txt file)"""
+
+import argparse
+import getpass
+import pathlib
+from time import time
+from argparse import ArgumentParser, Namespace, ArgumentError
+from typing import Any
+from tqdm import tqdm
+from os import walk
+from crypto import Decryption, Encryption, Append
+from cryptography.fernet import InvalidToken
+
+
+def invalid_name(value: str):
+    if value.endswith(('.txt', '.secret')):
+        return value
+    raise argparse.ArgumentError
+
+
+class Password(argparse.Action):
+    def __call__(self, parser, ArgumentParser, namespace: Namespace, values: Any, option_string)->None:
+        if values is None:
+            values = getpass.getpass()
+            setattr(namespace, self.dest, values)
+
+
+def list_file_to_process(dir_name: str):
+    to_file_process = []
+    for path, dirs, files in walk(dir_name):
+        for file in files:
+            if file.endswith(('.txt', '.secret')):
+                to_file_process.append(f'{path}\{file}')
+    return to_file_process
+
+    
+def main(args):
+    try:
+        if args.dir:
+            to_file_process = list_file_to_process(args.dir)
+        elif args.file:
+            to_file_process = args.file
+        else:
+            raise argparse.ArgumentError()
+        if args.verbose > 2:
+            to_file_process = tqdm(args.file)
+        for file in to_file_process:
+            before = time()
+            path = pathlib.Path(file)
+            if 'encrypt' == args.mode:
+                action = Encryption(path)
+            elif args.mode == 'decrypt':
+                action = Decryption(path)
+            elif args.mode == 'append':
+                text = input('Co dopisać do pliku? ')
+                action = Append(path, text)
+
+            action.verbosity = args.verbose
+            action.execute(args.password)
+            after = time()
+
+            if 3 > args.verbose > 0:
+                print(file + '  ', end='')
+                if args.verbose > 1:
+                    times = after-before
+                    print(f'Encrypt/Decrypt time is: {times:.3f}')
+                print()
+            if args.verbose > 2:
+                to_file_process.set_description(file)
+
+    except InvalidToken:
+        print('Niepoprawne hasło!')
+    except ArgumentError:
+        print("Nie podano pliku ani dir")
+
+
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser(description='Keep my secrets safe!!!',
+                                    formatter_class=argparse.RawTextHelpFormatter)
+    parse.add_argument('-m', '--mode', choices=['encrypt', 'decrypt', 'append'], required=True,
+                       help='''encrypt --> file encrypt
+    decrypt --> decryption file decryption
+    append --> append text to encrypted file''')
+
+    group = parse.add_mutually_exclusive_group()
+    group.add_argument('-f', '--file', action='append', type=invalid_name, help='Choose file')
+    group.add_argument('-d', '--dir', help='path to file process')
+
+    group1 = parse.add_mutually_exclusive_group()
+    group1.add_argument('-v', '--verbose', default=0, action='count', help='Verbose')
+    group1.add_argument('-q', '--quiet', default=False, action='store_true', help='Quiet')
+
+    parse.add_argument('-p', '--password', required=True, help='Enter password', nargs='?',
+                       dest='password', action=Password)
+
+    arg = parse.parse_args()
+
+    main(arg)
+
